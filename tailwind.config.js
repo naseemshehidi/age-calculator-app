6888b4ffd15a13b2f9530fea769b6161349464ab
naseem <shehidinaseem@gmail.com>
2023-07-23 23:08:40 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px", // Small screens and up
+        md: "768px", // Medium screens and up
+        lg: "1024px", // Large screens and up
+        xl: "1280px", // Extra large screens and up
+      },
+      colors: {
+        purple: "hsl(259, 100%, 65%)",
+        "light-red": "hsl(0, 100%, 67%)",
+        white: "hsl(0, 0%, 100%)",
+        "off-white": "hsl(0, 0%, 94%)",
+        "light-grey": "hsl(0, 0%, 86%)",
+        "smokey-grey": "hsl(0, 1%, 44%)",
+        "off-black": "hsl(0, 0%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
